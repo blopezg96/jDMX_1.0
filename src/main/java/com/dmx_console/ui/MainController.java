959de8a7f66fb6ae32b9e329361f9fc0ab210657
brrandonglock <121637085+brrandonglock@users.noreply.github.com>
2026-05-12 11:56:44 -0700

    PANEL CENTRAL Y PANEL IZQUIERDO creados

--- src/main/java/com/dmx_console/ui/MainController.java
+++ src/main/java/com/dmx_console/ui/MainController.java
@@ -1,8 +1,13 @@
 package com.dmx_console.ui;
 
 import com.dmx_console.model.Fixture;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 import com.dmx_console.service.FixtureService;
+import javafx.scene.layout.VBox;
 
 import java.util.List;
 
@@ -11,6 +16,8 @@ public class MainController {
     private final List<Fixture> rig;
     private final FixtureService service;
     private final BorderPane view;
+
+    private Fixture selectedFixture;
 
     public MainController(List<Fixture> rig, FixtureService service){
 
@@ -21,10 +28,45 @@ public class MainController {
     }
 
     private void buildUI(){
-        // aqui construimos la UI paso a paso
+
+        // Modelar panel izquierdo - Lista de Fixtures
+        ListView<String> fixtureList = new ListView<>();
+        for (Fixture f: rig){
+            fixtureList.getItems().add(f.getName());
+        }
+        fixtureList.setPrefWidth(160);
+
+        // modelando panel central - sliders de cada canal
+        VBox sliderPanel = new VBox(12);
+        sliderPanel.setPadding(new Insets(20));
+
+        Label labelFixture = new Label("Select Fixture");
+        labelFixture.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+
+        Slider sliderR = createSlider();
+        Slider sliderG = createSlider();
+        Slider sliderB = createSlider();
+        Slider sliderW = createSlider();
+        Slider sliderY = createSlider();
+        Slider sliderStrobe = createSlider();
+
+        // Preview del color
+
+
+    }
+
+    public Slider createSlider(){
+        Slider slider = new Slider(0, 255, 0);
+        slider.setShowTickMarks(true);
+        slider.setShowTickLabels(true);
+        slider.setMajorTickUnit(64);
+        slider.setPrefWidth(400);
+        return slider;
     }
 
     public BorderPane getView(){
         return view;
     }
 }
+
+
